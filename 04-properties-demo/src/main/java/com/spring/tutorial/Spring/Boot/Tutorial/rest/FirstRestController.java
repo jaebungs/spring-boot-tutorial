d2b9344ba8cb07fe87d855f1f20f1c48c9b0b160
jaebungs <jaebungs@gmail.com>
2023-06-18 16:07:00 -0400

    play with custom properties (like .env)

--- 04-properties-demo/src/main/java/com/spring/tutorial/Spring/Boot/Tutorial/rest/FirstRestController.java
+++ 04-properties-demo/src/main/java/com/spring/tutorial/Spring/Boot/Tutorial/rest/FirstRestController.java
@@ -3,15 +3,29 @@ package com.spring.tutorial.Spring.Boot.Tutorial.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
-// @RequestMapping("/rest") Parent mapping
 public class FirstRestController {
-    // expose "/" that return "Hello World"
- 
-    @GetMapping("/") // Child mapping
+
+    // inject properteis for "coach.name" and "team.name"
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/") 
     public String sayHello() {
         return "Hello World";
+    }
+
+    // expose new endpoint for "teaminfo"
+    // http://localhost:8080/teaminfo
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
     }
 }
 
